b187b37c028b5d859bd26947cfb5874692408a25
Rodridls22 <rodrigodelossantos3101@gmail.com>
2024-07-18 23:15:56 -0300

    Delete SEGUNDA ENTREGA FINAL directory

--- SEGUNDA ENTREGA FINAL/STORED PROCEDURE/ActualizarDeliverySP.sql
+++
@@ -1,9 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ActualizarDelivery`(
-    IN p_IDLoc INT,
-    IN p_TieneDelivery BOOLEAN
-)
-BEGIN
-    UPDATE Local
-    SET Delivery = p_TieneDelivery
-    WHERE ID = p_IDLoc;
-END
